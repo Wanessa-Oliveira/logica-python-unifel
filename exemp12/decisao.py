26003e4980c4dd8b4b4b9f35f95ab7b139d7cef6
Wanessa dev <wanessafo1982@gmail.com>
2026-04-26 00:36:30 -0300

    exemp 2 modificado e 14 gerado

--- exemp12/decisao.py
+++ exemp12/decisao.py
@@ -16,3 +16,40 @@ if usuario in convidados:
 else:
     print("ACESSO NEGADO. Seu nome não consta na lista. Procure a rececpção!")
 print("-" * 30)
+
+
+
+# # Caso 2 -> Classificação do clima do evento
+# print("\n" * 1)
+# temp = float(input("Digite a temperatura atual da sala: "))
+# status = None
+
+# if temp > 35:
+#     status = "Calor extremo no salão"
+# elif 25 <= temp <= 35:  # Encadeamento lógico -> temp >= 25 and temp <= 35
+#     status = "Suportável"
+# elif 17 <= temp <= 24:
+#     status = "Agradável"
+# else:
+#     status = "Muito Frio"  # 16 pra baixo
+#     print(f"A temperatura está: {temp} (16 ou abaixo)!! \n")
+
+# # print(f"O clima está: {status} ({temp}º)!! \n")
+
+
+"""
+Comentário de múltiplas linhas
+"""
+
+# Comentário de uma linha
+
+# CASO 3: Menu de comandos da gestão do evento
+opcao = input("Escolha uma opção: A (Relatório) | B (Saldo) | C (Sair): ").upper()
+
+match opcao:
+    case "A":
+        print("Gerando relatório detalhado do evento... \n")
+    case "B":
+        print("Calculando a receita consolidada atual do evento... \n")
+    case "C":
+        print("Saindo do sistema... Até logo! \n")
---
+++ exemp14/app.py
@@ -0,0 +1,31 @@
+# exemp_xx (login) :rie duas variaveis user_db = "admin" e pass_db - " 123"
+# Recwb via iput o usuario e senha e exiba true se ambos baterem.
+
+user_db = "admin"
+pass_db = "123"
+
+usuario = input("Digite o usuário: ")
+senha = input("Digite a senha: ")
+
+acesso = usuario == user_db and senha == pass_db
+
+print(acesso)
+
+# exemp_xx (Sensor) : Crie um sistema que exiba True se uma temperatura estiver
+# entre 20 e 30 graus (faixa ideal). Receba a temperatura do usuario.
+
+temperatura = float(input("Digite a temperatura: "))
+
+ideal = temperatura >= 20 and temperatura <= 30
+
+print(ideal)
+
+# exemp_x (Elegibilidade): Um eleitor só vota se tiver idade entre 16 e 70 anos.
+# Receba do usuario a idade, e então
+# exiba True ou False para o candidato (teste candidatos com 15 anos,
+# e tambem com 75 anos para ver seo código funciona)
+idade = int(input("Digite a idade: "))
+
+pode_votar = idade >= 16 and idade <= 70
+
+print(pode_votar)
